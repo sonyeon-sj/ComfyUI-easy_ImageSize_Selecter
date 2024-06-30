d73405cb046b0c88bd73e52fca53c1a69928fdfc
sonyeon-sj <gene0910@gmail.com>
2024-06-30 17:41:40 +0900

    first upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# ComfyUI-easy-ImageSize-Selecter
+
+---
+
+First of all, I tell you that I don't know how to programming. I just made it.
+However, it works well.
+
+---
+
+**Custom node for ComfyUI**
+Select the image size from the preset and select Vertical and Horizontal to output Width and Height.
+
+In comfyUI, there is no use except for the 7 sizes shown here.This is because the models learned at a fixed size.
+You just have to decide whether it's horizontal or vertical
+
+### How to use it
+
+![screenshot1](./images/screenshot01.png)
+just select presets (size & direction)
+Then, it will export the width and height as INT.
+
+In comfyUI, there are a lot of image size input fields. You can use it after pulling it.
+
+![screenshot1](./images/screenshot03.png)
+Incase of nodes with built-in width and height inputs:
+Right-click the mouse > Convert Widget to Input > Convert width(height) to input
+![screenshot1](./images/screenshot04.png)
---
+++ easy_ImageSize_Selecter.py
@@ -0,0 +1,58 @@
+
+class ImageSizer:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model_type": (["SD_1:1_512*512","SD_2:3_512*768","XL_1:1_1024*1024","XL_2:3_832*1216","XL_16:9_1344*768","XL_21:9_1536*640","SVD_1024*576"],{"default": "XL_2:3_832*1216"}),
+                "direction": ([ "Vertical","Horizon"],{"default": "Vertical"})
+            }
+        }
+
+    RETURN_TYPES = ("INT","INT",)
+    RETURN_NAMES = ("Width", "Height",)
+
+    FUNCTION = "run"
+
+    CATEGORY = "SizeFromPresets"
+    OUTPUT_NODE = True
+
+    def run(self, model_type,direction):
+        find_indexList = ["SD_1:1_512*512","SD_2:3_512*768","XL_1:1_1024*1024","XL_2:3_832*1216","XL_16:9_1344*768","XL_21:9_1536*640","SVD_1024*576"]
+        find_resolList = [(512,512),(512,768),(1024,1024),(832,1216),(1344,768),(1536,640),(1024,576)]
+        width, height= find_resolList[find_indexList.index(model_type)]
+        if direction == "Vertical":
+            if width > height: 
+                new_width = height
+                height = width
+                width = new_width
+            else:
+                pass
+        else: #horizon
+            if width < height: 
+                new_width = height
+                height = width
+                width = new_width
+            else:
+                pass
+        text1 = str(width)+' x '+str(height)
+        #return (int(round(width)), int(round(height)),{"ui": {"text": text, f"SetResolution: {text}"})
+        return {
+            "ui": {"text": (text1,f"{str(width)} X {str(height)}")
+                }, 
+            "result": (
+                int(round(width)),
+                int(round(height)),
+                ),
+        }
+    
+NODE_CLASS_MAPPINGS = {
+    "ImageSizer": ImageSizer
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ImageSizer": "easy ImageSize Selecter"
+}
