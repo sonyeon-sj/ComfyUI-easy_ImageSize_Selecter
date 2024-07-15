8cca7be2e88a4ef1236713dfd3f643b41c83c501
sonyeon-sj <gene0910@gmail.com>
2024-07-15 13:44:08 +0900

    fix path

--- easy_ImageSize_selecter/__init__.py
+++ easy_ImageSize_selecter/__init__.py
@@ -1,3 +1,3 @@
-from .easy_ImageSize_Selecter import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from ./easy_ImageSize_selecter/easy_ImageSize_Selecter import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
