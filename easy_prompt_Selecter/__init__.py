dbf0e4f94480c35f6ae8d99e4cf737742aa8a49f
sonyeon-sj <gene0910@gmail.com>
2024-07-15 15:41:07 +0900

    fix initpath

--- easy_prompt_Selecter/__init__.py
+++ easy_prompt_Selecter/__init__.py
@@ -1,3 +1,3 @@
-from ./easy_prompt_Selecter/easy_prompt_Selecter import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .easy_prompt_Selecter.easy_prompt_Selecter import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
